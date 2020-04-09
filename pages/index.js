f4503ae3b0a7b8f97b348ad6ee00e6018175cc89
Huy Quang Do <>
2020-04-09 19:35:37 +0700

    Improve homepage

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,49 @@
 import Head from "next/head"
 import { Component } from 'react'
-import { attributes, react as HomeContent } from '../content/home.md';
+import Link from 'next/link';
+
+const BLOG_POSTS_PATH = '../content/blogs';
+
+const importBlogPosts = async () => {
+  const markdownFiles = require
+    .context('../content/blogs', false, /\.md$/)
+    .keys()
+    .map(relativePath => relativePath.substring(2));
+  return Promise.all(
+    markdownFiles.map(async path => {
+      const markdown = await import(`../content/blogs/${path}`);
+      return { ...markdown, slug: path.substring(0, path.length - 3) };
+    })
+  );
+};
 
 export default class Home extends Component {
+
+  static async getInitialProps() {
+    const postsList = await importBlogPosts();
+
+    return { postsList };
+  }
+
   render() {
-    let { title, cats } = attributes;
+    const { postsList } = this.props;
     return (
       <>
         <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </Head>
         <article>
-          <h1>{title}</h1>
-          <HomeContent />
-          <ul>
-            {cats.map((cat, k) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-              </li>
-            ))}
-          </ul>
+          {postsList.map(post => {
+            return (
+              <Link href={`blog/post/${post.slug}`}>
+                <a>
+                  <img src={post.attributes.thumbnail} />
+                  <h2>{post.attributes.title}</h2>
+                </a>
+              </Link>
+            );
+          })}
+
         </article>
       </>
     )
